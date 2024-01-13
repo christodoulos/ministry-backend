37b125c7e5997436338dfb5fb8a0ceca7649dec0
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-01-13 23:44:22 +0200

    Dictionary swagger

--- src/blueprints/apografi.py
+++ src/blueprints/apografi.py
@@ -7,7 +7,7 @@ apografi = Blueprint("apografi", __name__)
 
 
 @apografi.route("/dictionary", methods=["POST"])
-def post_lejiko():
+def post_dictionary():
     post_data = request.get_json()
     data = {
         "apografi_id": post_data["id"],
@@ -15,19 +15,44 @@ def post_lejiko():
         "code_el": post_data["code_el"],
         "description": post_data["description"],
     }
-    lejiko = Dictionary(**data)
-    lejiko.save()
+    dictionary = Dictionary(**data)
+    dictionary.save()
     return Response(json.dumps(data), mimetype="application/json")
+
+
+@apografi.route("/dictionary/<string:dictionary>/<int:id>/description", methods=["GET"])
+def get_dictionary_id(dictionary: str, id: int):
+    try:
+        doc = Dictionary.objects().get(code=dictionary, apografi_id=id)
+        description = {"description": doc["description"]}
+        return Response(
+            json.dumps(description), mimetype="application/json", status=200
+        )
+    except Exception as e:
+        error = {"error": str(e)}
+        return Response(json.dumps(error), mimetype="application/json", status=404)
+
+
+@apografi.route( "/dictionary/<string:dictionary>/<string:description>/id", methods=["GET"] )  # fmt: skip
+def get_dictionary_code(dictionary: str, description: str):
+    try:
+        doc = Dictionary.objects().get(code=dictionary, description=description)
+        print(doc)
+        id = {"id": doc["id"]}
+        return Response(json.dumps(id), mimetype="application/json", status=200)
+    except Exception as e:
+        error = {"error": str(e)}
+        return Response(json.dumps(error), mimetype="application/json", status=404)
 
 
 @apografi.route("/dictionary/<string:dictionary>", methods=["GET"])
 def get_dictionary(dictionary: str):
-    lejika = Dictionary.objects(code=dictionary)
-    return Response(lejika.to_json(), mimetype="application/json")
+    dictionary = Dictionary.objects(code=dictionary)
+    return Response(dictionary.to_json(), mimetype="application/json", status=200)
 
 
 @apografi.route("/dictionary/<string:dictionary>/ids", methods=["GET"])
 def get_dictionary_ids(dictionary: str):
     docs = Dictionary.objects(code=dictionary)
     ids = [doc["id"] for doc in docs]
-    return Response(json.dumps(ids), mimetype="application/json")
+    return Response(json.dumps(ids), mimetype="application/json", status=200)
