8e9271afe51e7963baa87e91a70c00aaefbad2e3
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-01-15 22:51:58 +0200

    deploy, supervisor

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -avz --delete requirements.txt src wsgi.py ypes-backend:flask
 ssh ypes-backend 'source ~/flask/venv/bin/activate && pip install -r ~/flask/requirements.txt'
-ssh ypes-backend 'systemctl --user restart backend'
+ssh oper@lola 'sudo supervisorctl reload'
