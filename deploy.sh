1264540e0518ffbdd2f813d71dfcf4d66f84d671
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-01-28 12:56:16 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avz --delete apografi_sync.py requirements.txt src wsgi.py ypes-backend:flask
+rsync -avz --delete apografi_sync*.py count_distict.py requirements.txt src wsgi.py ypes-backend:flask
 ssh ypes-backend 'source ~/flask/venv/bin/activate && pip install -r ~/flask/requirements.txt'
 ssh oper@lola 'sudo supervisorctl reload'
