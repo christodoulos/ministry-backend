9289fa344ecb76226f50ecdb800d59e67e47fad7
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-10 21:01:11 +0200

    cofog from word file bliax

---
+++ src/models/cofog.py
@@ -0,0 +1,20 @@
+import mongoengine as me
+
+
+class Cofog3(me.EmbeddedDocument):
+    code = me.StringField(required=True)
+    name = me.StringField(required=True)
+
+
+class Cofog2(me.EmbeddedDocument):
+    code = me.StringField(required=True)
+    name = me.StringField(required=True)
+    cofog3 = me.ListField(me.EmbeddedDocumentField(Cofog3), required=True)
+
+
+class Cofog1(me.Document):
+    code = me.StringField(required=True)
+    name = me.StringField(required=True)
+    cofog2 = me.ListField(me.EmbeddedDocumentField(Cofog2), required=True)
+
+    meta = {"collection": "cofog", "db_alias": "psped"}
