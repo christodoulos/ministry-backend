2df1b637cb40fad89d01a44a48cc816828acd0b3
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-21 05:56:31 +0200

    Ruff formatting

--- src/blueprints/armodiotites.py
+++ src/blueprints/armodiotites.py
@@ -7,38 +7,37 @@ from src.models.apografi.organizational_unit import OrganizationalUnit
 
 remit = Blueprint("armodiotites", __name__)
 
+
 @remit.route("/remit", methods=["POST"])
 def create_remit():
     try:
         # Assume data contains all required fields except those that are auto-generated (shouldnt we check that?)
         data = request.get_json()
-        # check if organization unit code exists 
-        try: 
-            unitcode = OrganizationalUnit.objects.get(code=data['unitCode'])
+        # check if organization unit code exists
+        try:
+            unitcode = OrganizationalUnit.objects.get(code=data["unitCode"])
         except OrganizationalUnit.DoesNotExist:
-            return Response(json.dumps({
-                    "error": f"Δεν βρέθηκε μονάδα με κωδικό {data['unitCode']}"}),
-                    mimetype="application/json",
-                    status=404
+            return Response(
+                json.dumps({"error": f"Δεν βρέθηκε μονάδα με κωδικό {data['unitCode']}"}),
+                mimetype="application/json",
+                status=404,
             )
         # check if diataxeis codes exists
-        for diataxi_code in data['diataxisCodes']:
+        for diataxi_code in data["diataxisCodes"]:
             try:
                 diataxi = Diataxi.objects.get(legalProvisionCode=diataxi_code)
             except Diataxi.DoesNotExist:
-                return Response(json.dumps({
-                    "error": f"Δεν βρέθηκε νομική διάταξη με κωδικό {diataxi_code}"}),
+                return Response(
+                    json.dumps({"error": f"Δεν βρέθηκε νομική διάταξη με κωδικό {diataxi_code}"}),
                     mimetype="application/json",
-                    status=404
+                    status=404,
                 )
         # create remit code
-        data['remitCode'] = Remit.generate_remit_code()
+        data["remitCode"] = Remit.generate_remit_code()
         # create a remit & save it
-        remit = Remit(**data) 
+        remit = Remit(**data)
         remit.save()
-        return Response(remit.to_json(),
-                        mimetype="application/json",
-                        status=200)
+        return Response(remit.to_json(), mimetype="application/json", status=200)
 
     except Exception as e:
         return Response(
@@ -46,6 +45,7 @@ def create_remit():
             mimetype="application/json",
             status=500,
         )
+
 
 @remit.route("/remits/", methods=["GET"])
 def retrieve_all_remit():
@@ -56,9 +56,9 @@ def retrieve_all_remit():
         status=200,
     )
 
+
 @remit.route("/remits/<string:remitCode>", methods=["GET"])
 def retrieve_armodiotita(remitCode: str):
-
     try:
         remit = Remit.objects.get(remitCode=remitCode)
         return Response(
@@ -68,8 +68,7 @@ def retrieve_armodiotita(remitCode: str):
         )
     except Remit.DoesNotExist:
         return Response(
-            json.dumps(
-                {"error": (f"Δεν βρέθηκε αρμοδιότητα με κωδικό {remitCode}")}),
+            json.dumps({"error": (f"Δεν βρέθηκε αρμοδιότητα με κωδικό {remitCode}")}),
             mimetype="application/json",
             status=404,
         )
@@ -82,52 +81,45 @@ def update_remit(remitCode: str):
         remit = Remit.objects.get(remitCode=remitCode)
     except Remit.DoesNotExist:
         return Response(
-            json.dumps(
-                {"error": f"Δεν βρέθηκε αρμοδιότητα με κωδικό {remitCode}"}),
+            json.dumps({"error": f"Δεν βρέθηκε αρμοδιότητα με κωδικό {remitCode}"}),
             mimetype="application/json",
             status=404,
         )
 
     # check for the immutable fields
-    immutable_fields: list = [
-        'remitCode',
-        'creationDate',
-        'userCode'
-    ]
+    immutable_fields: list = ["remitCode", "creationDate", "userCode"]
     update_fields = data.keys()
 
     if any(field in update_fields for field in immutable_fields):
-        return Response(json.dumps({
-            "error":
-            "Μη επιτρεπτή ενημέρωση πεδίων: remitCode, creationDate, userCode"
-        }),
-                        mimetype="application/json",
-                        status=400)
+        return Response(
+            json.dumps({"error": "Μη επιτρεπτή ενημέρωση πεδίων: remitCode, creationDate, userCode"}),
+            mimetype="application/json",
+            status=400,
+        )
 
     # check that Organization unit code exists
-    unit_code = data.get('unitCode')
+    unit_code = data.get("unitCode")
     if unit_code:
         try:
             org_unit = OrganizationalUnit.objects.get(code=unit_code)
         except OrganizationalUnit.DoesNotExist:
-            return Response(json.dumps({
-                "error":
-                f"Δεν βρέθηκε Μονάδα με κωδικό {unit_code}"
-            }),
-                            mimetype="application/json",
-                            status=404)
-        
+            return Response(
+                json.dumps({"error": f"Δεν βρέθηκε Μονάδα με κωδικό {unit_code}"}),
+                mimetype="application/json",
+                status=404,
+            )
+
     # check that diataxeis codes exist
-    diataxis_codes = data.get('diataxisCodes')
+    diataxis_codes = data.get("diataxisCodes")
     if diataxis_codes:
         for code in diataxis_codes:
-            try: 
+            try:
                 diataxi = Diataxi.objects.get(legalProvisionCode=code)
             except Diataxi.DoesNotExist:
-                return Response(json.dumps({
-                    "error": f"Δεν βρέθηκε νομική διάταξη με κωδικό {code}"}),
+                return Response(
+                    json.dumps({"error": f"Δεν βρέθηκε νομική διάταξη με κωδικό {code}"}),
                     mimetype="application/json",
-                    status=404
+                    status=404,
                 )
 
     # Manually update each field
@@ -137,9 +129,7 @@ def update_remit(remitCode: str):
                 setattr(remit, key, value)
 
         remit.save()  # This will now perform validation and other logic
-        return Response(remit.to_json(),
-                        mimetype="application/json",
-                        status=200)
+        return Response(remit.to_json(), mimetype="application/json", status=200)
     except Exception as e:
         return Response(
             json.dumps({"error": f"Αποτυχία ενημέρωσης αρμοδιότητας: {e}"}),
@@ -153,12 +143,14 @@ def delete_remit(remitCode: str):
     try:
         remit = Remit.objects.get(remitCode=remitCode)
         remit.detele()
-        return Response(json.dumps(
-            {"success": f"Η αρμοδιότητα με τον κωδικό {remitCode} διαγράφηκε"}),
-                        mimetype="application/json",
-                        status=200)
+        return Response(
+            json.dumps({"success": f"Η αρμοδιότητα με τον κωδικό {remitCode} διαγράφηκε"}),
+            mimetype="application/json",
+            status=200,
+        )
     except Remit.DoesNotExist:
-        return Response(json.dumps(
-            {"error": f"Δεν βρέθηκε αρμοδιότητα με τον κωδικό: {remitCode}"}),
-                        mimetype="application/json",
-                        status=404)
+        return Response(
+            json.dumps({"error": f"Δεν βρέθηκε αρμοδιότητα με τον κωδικό: {remitCode}"}),
+            mimetype="application/json",
+            status=404,
+        )
