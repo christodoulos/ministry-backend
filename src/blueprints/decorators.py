a6aeb8f84cfd127fa9f30177edfec29cfdeb1053
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-23 04:48:02 +0200

    put decorator

---
+++ src/blueprints/decorators.py
@@ -0,0 +1,28 @@
+from flask import request, Response
+from flask_jwt_extended import get_jwt_identity, get_jwt
+from functools import wraps
+import json
+
+
+def can_edit(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        current_user = get_jwt_identity()
+        claims = get_jwt()
+        print(current_user, claims)
+
+        user_roles = claims["roles"]
+        type_roles = [x for x in user_roles if x["role"] in ["EDITOR", "ADMIN", "ROOT"]]
+        code = kwargs.get("code", "")
+        type_roles = [x for x in type_roles if code in x["foreas"] or code in x["monades"]]
+
+        if not type_roles:
+            return Response(
+                json.dumps({"error": f"Δεν επιτρέπεται η αλλαγή του φορέα {code}"}),
+                mimetype="application/json",
+                status=403,
+            )
+
+        return f(*args, **kwargs)
+
+    return decorated_function
--- src/blueprints/psped.py
+++ src/blueprints/psped.py
@@ -3,6 +3,7 @@ from flask import Blueprint, Response, request
 from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
 
 from src.models.psped import Foreas
+from src.blueprints.decorators import can_edit
 
 psped = Blueprint("psped", __name__)
 
@@ -25,39 +26,59 @@ def get_foreas(code: str):
         )
 
 
+# @psped.route("/foreas/<string:code>", methods=["PUT"])
+# @jwt_required()
+# def update_poliepipedi(code: str):
+#     current_user = get_jwt_identity()
+#     claims = get_jwt()
+#     print(current_user, claims)
+
+#     user_roles = claims["roles"]
+#     type_roles = [x for x in user_roles if x["role"] in ["EDITOR", "ADMIN", "ROOT"]]
+#     type_roles = [x for x in type_roles if code in x["foreas"] or code in x["monades"]]
+
+#     if type_roles:
+#         try:
+#             data = request.get_json()
+#             foreas = Foreas.objects.get(code=data["code"])
+#             foreas.update(**data)
+#             return Response(
+#                 json.dumps(foreas.to_json()),
+#                 mimetype="application/json",
+#                 status=200,
+#             )
+#         except Foreas.DoesNotExist:
+#             return Response(
+#                 json.dumps({"error": f"Δεν βρέθηκε φορέας με κωδικό {code}"}),
+#                 mimetype="application/json",
+#                 status=404,
+#             )
+
+
+#     else:
+#         return Response(
+#             json.dumps({"error": f"Δεν επιτρέπεται η αλλαγή του φορέα {code}"}),
+#             mimetype="application/json",
+#             status=403,
+#         )
 @psped.route("/foreas/<string:code>", methods=["PUT"])
 @jwt_required()
+@can_edit
 def update_poliepipedi(code: str):
-    current_user = get_jwt_identity()
-    claims = get_jwt()
-    print(current_user, claims)
-
-    user_roles = claims["roles"]
-    type_roles = [x for x in user_roles if x["role"] in ["EDITOR", "ADMIN", "ROOT"]]
-    type_roles = [x for x in type_roles if code in x["foreas"] or code in x["monades"]]
-
-    if type_roles:
-        try:
-            data = request.get_json()
-            foreas = Foreas.objects.get(code=data["code"])
-            foreas.update(**data)
-            return Response(
-                json.dumps(foreas.to_json()),
-                mimetype="application/json",
-                status=200,
-            )
-        except Foreas.DoesNotExist:
-            return Response(
-                json.dumps({"error": f"Δεν βρέθηκε φορέας με κωδικό {code}"}),
-                mimetype="application/json",
-                status=404,
-            )
-
-    else:
+    try:
+        data = request.get_json()
+        foreas = Foreas.objects.get(code=data["code"])
+        foreas.update(**data)
         return Response(
-            json.dumps({"error": f"Δεν επιτρέπεται η αλλαγή του φορέα {code}"}),
+            json.dumps(foreas.to_json()),
             mimetype="application/json",
-            status=403,
+            status=200,
+        )
+    except Foreas.DoesNotExist:
+        return Response(
+            json.dumps({"error": f"Δεν βρέθηκε φορέας με κωδικό {code}"}),
+            mimetype="application/json",
+            status=404,
         )
 
 
